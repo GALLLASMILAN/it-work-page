df47c1baf2724bf11e182255563e236efab74842
Milan Gallas <gallas.milan@gmaiil.com>
2018-11-03 15:51:45 +0100

    prepare user router

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,10 @@
 const express = require('express');
 import { Express } from 'express';
 const app: Express = express();
+const bodyParser = require('body-parser');
 
-import userRouter from './routes/user';
+import userRouter from './routes/user/';
 
+app.use(bodyParser.json());
 app.use('/user', userRouter);
 app.listen(3000);
